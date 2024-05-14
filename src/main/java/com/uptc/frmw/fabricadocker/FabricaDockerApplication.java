8517fbf020b3f5b49702e106b7a534b1afb4a2d9
Juan Manuel Baracaldo Rojas <87029593+juan-Baracaldo-rojas@users.noreply.github.com>
2024-05-14 11:57:36 -0500

    creacion proyecto

---
+++ src/main/java/com/uptc/frmw/fabricadocker/FabricaDockerApplication.java
@@ -0,0 +1,13 @@
+package com.uptc.frmw.fabricadocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FabricaDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FabricaDockerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uptc/frmw/fabricadocker/FabricaDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uptc.frmw.fabricadocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FabricaDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
