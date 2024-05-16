8969f6bbcd0bdea997d34bd6092bb7e356383a77
JonyALopez <93666131+JonyALopez@users.noreply.github.com>
2024-05-16 18:56:42 -0500

    CRUD Machine
    Se crean las peicones http para hacer el crud de las maquinas

--- src/main/java/com/uptc/frmw/fabricadocker/Controller/MachineController.java
+++ src/main/java/com/uptc/frmw/fabricadocker/Controller/MachineController.java
@@ -3,9 +3,7 @@ package com.uptc.frmw.fabricadocker.Controller;
 import com.uptc.frmw.fabricadocker.Jpa.Models.Machine;
 import com.uptc.frmw.fabricadocker.Services.MachineService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -17,5 +15,23 @@ public class MachineController {
     @GetMapping
     public List<Machine> getAllMachine() {
         return machineService.findAllMachine();
+    }
+
+    @GetMapping("/{idMachine}")
+    public Machine getByIdMachine (@PathVariable  Integer idMachine) {
+        return machineService.findByIdMachune(idMachine);
+    }
+    @DeleteMapping("/{idMachine}")
+    public void deleteMachine (@PathVariable Integer idMachine) {
+        machineService.deleteMachine(idMachine);
+    }
+    @PostMapping
+    public Machine saveMachine (@RequestBody Machine machine) {
+        return machineService.saveMachine(machine);
+    }
+
+    @PutMapping("/{idMachine}")
+    public Machine updateMachine (@PathVariable Integer idMachine, @RequestBody Machine machine) {
+        return machineService.updateMachine(idMachine, machine);
     }
 }
